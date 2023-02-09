2d40d55d9a9470d3b4c6847c1c7b1c0bbeaa8ebe
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-09 08:34:33 +0530

    added: attendance functionality

---
+++ EmployeeCheck.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class EmployeeCheck
+    {
+        public static void CheckEmployee()
+        {
+            int present = 1;
+            Random random = new Random();
+            int empCheck = random.Next(2);
+
+            if(empCheck == present)
+            {
+                Console.WriteLine("Employee is Present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}

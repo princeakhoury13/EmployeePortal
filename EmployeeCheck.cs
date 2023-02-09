09e4bb08c6d1ee003006559ece7003cfd5b96d66
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-09 08:41:18 +0530

    added: check daily wage functionality

--- EmployeeCheck.cs
+++ EmployeeCheck.cs
@@ -11,18 +11,26 @@ namespace EmployeeWageComputation
         public static void CheckEmployee()
         {
             int present = 1;
+            int hourlyWage = 20;
+            int empHours = 0;
+            int dailyWage = 0;
+
             Random random = new Random();
             int empCheck = random.Next(2);
 
             if(empCheck == present)
             {
                 Console.WriteLine("Employee is Present");
+                empHours = 8;
 
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
+                empHours = 0;
             }
+            dailyWage = empHours * hourlyWage;
+            Console.WriteLine("Employee's Wage Today: "+ dailyWage);
         }
     }
 }

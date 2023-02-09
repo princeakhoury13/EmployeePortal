63daef801ec0c57380688e957028e0f532bf13a8
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-09 08:33:37 +0530

    added: calling the check attendance function

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Employees Portal!");
-            
+            EmployeeCheck.CheckEmployee();
         }
     }
     

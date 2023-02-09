3482eaec12f3cab6d8733d4266c6aeb4a11681e8
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-09 08:31:41 +0530

    added: Initial Greeting Message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeWageComputation
+{
+    class Greet
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Employees Portal!");
+            
+        }
+    }
+    
+}
